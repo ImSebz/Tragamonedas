556c77b0ae6d91a5905a15e97ce29f8d488212a1
Juan Sebastian Camargo Prieto <juansecapri@hotmail.com>
2021-02-23 19:22:48 -0500

    Tragamonedas

---
+++ MainJuego.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace JuegoTragamonedas
+{
+    partial class MainJuego
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainJuego));
+            this.panelJuego1 = new System.Windows.Forms.Panel();
+            this.panelJuego3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.panelJuego2 = new System.Windows.Forms.Panel();
+            this.imagenBotonPlay = new System.Windows.Forms.PictureBox();
+            this.pictureBoxStop3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxStop2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxStop1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxHelp = new System.Windows.Forms.PictureBox();
+            this.pictureBoxExit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenBotonPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStop3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStop2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStop1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHelp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelJuego1
+            // 
+            this.panelJuego1.BackColor = System.Drawing.Color.Transparent;
+            this.panelJuego1.Location = new System.Drawing.Point(250, 175);
+            this.panelJuego1.Name = "panelJuego1";
+            this.panelJuego1.Size = new System.Drawing.Size(195, 190);
+            this.panelJuego1.TabIndex = 1;
+            // 
+            // panelJuego3
+            // 
+            this.panelJuego3.BackColor = System.Drawing.Color.Transparent;
+            this.panelJuego3.Location = new System.Drawing.Point(699, 175);
+            this.panelJuego3.Name = "panelJuego3";
+            this.panelJuego3.Size = new System.Drawing.Size(195, 190);
+            this.panelJuego3.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // panelJuego2
+            // 
+            this.panelJuego2.BackColor = System.Drawing.Color.Transparent;
+            this.panelJuego2.Location = new System.Drawing.Point(479, 175);
+            this.panelJuego2.Name = "panelJuego2";
+            this.panelJuego2.Size = new System.Drawing.Size(195, 190);
+            this.panelJuego2.TabIndex = 2;
+            // 
+            // imagenBotonPlay
+            // 
+            this.imagenBotonPlay.BackColor = System.Drawing.Color.Transparent;
+            this.imagenBotonPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imagenBotonPlay.Image = global::JuegoTragamonedas.Properties.Resources.Play_Blanco;
+            this.imagenBotonPlay.Location = new System.Drawing.Point(879, 459);
+            this.imagenBotonPlay.Name = "imagenBotonPlay";
+            this.imagenBotonPlay.Size = new System.Drawing.Size(252, 84);
+            this.imagenBotonPlay.TabIndex = 7;
+            this.imagenBotonPlay.TabStop = false;
+            this.imagenBotonPlay.Click += new System.EventHandler(this.imagenBotonPlay_Click);
+            // 
+            // pictureBoxStop3
+            // 
+            this.pictureBoxStop3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxStop3.BackgroundImage = global::JuegoTragamonedas.Properties.Resources.Stop_Blanco;
+            this.pictureBoxStop3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxStop3.Image = global::JuegoTragamonedas.Properties.Resources.Stop_Blanco;
+            this.pictureBoxStop3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxStop3.InitialImage")));
+            this.pictureBoxStop3.Location = new System.Drawing.Point(699, 376);
+            this.pictureBoxStop3.Name = "pictureBoxStop3";
+            this.pictureBoxStop3.Size = new System.Drawing.Size(197, 77);
+            this.pictureBoxStop3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxStop3.TabIndex = 6;
+            this.pictureBoxStop3.TabStop = false;
+            this.pictureBoxStop3.Click += new System.EventHandler(this.pictureBoxStop3_Click);
+            // 
+            // pictureBoxStop2
+            // 
+            this.pictureBoxStop2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxStop2.BackgroundImage = global::JuegoTragamonedas.Properties.Resources.Stop_Blanco;
+            this.pictureBoxStop2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxStop2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxStop2.InitialImage")));
+            this.pictureBoxStop2.Location = new System.Drawing.Point(479, 376);
+            this.pictureBoxStop2.Name = "pictureBoxStop2";
+            this.pictureBoxStop2.Size = new System.Drawing.Size(197, 77);
+            this.pictureBoxStop2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxStop2.TabIndex = 5;
+            this.pictureBoxStop2.TabStop = false;
+            this.pictureBoxStop2.Click += new System.EventHandler(this.pictureBoxStop2_Click);
+            // 
+            // pictureBoxStop1
+            // 
+            this.pictureBoxStop1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxStop1.BackgroundImage = global::JuegoTragamonedas.Properties.Resources.Stop_Blanco;
+            this.pictureBoxStop1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxStop1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxStop1.InitialImage")));
+            this.pictureBoxStop1.Location = new System.Drawing.Point(252, 376);
+            this.pictureBoxStop1.Name = "pictureBoxStop1";
+            this.pictureBoxStop1.Size = new System.Drawing.Size(196, 77);
+            this.pictureBoxStop1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxStop1.TabIndex = 4;
+            this.pictureBoxStop1.TabStop = false;
+            this.pictureBoxStop1.Click += new System.EventHandler(this.pictureBoxStop1_Click);
+            // 
+            // pictureBoxHelp
+            // 
+            this.pictureBoxHelp.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxHelp.BackgroundImage = global::JuegoTragamonedas.Properties.Resources.Help;
+            this.pictureBoxHelp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxHelp.Location = new System.Drawing.Point(49, 459);
+            this.pictureBoxHelp.Name = "pictureBoxHelp";
+            this.pictureBoxHelp.Size = new System.Drawing.Size(223, 84);
+            this.pictureBoxHelp.TabIndex = 8;
+            this.pictureBoxHelp.TabStop = false;
+            this.pictureBoxHelp.Click += new System.EventHandler(this.pictureBoxHelp_Click);
+            // 
+            // pictureBoxExit
+            // 
+            this.pictureBoxExit.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxExit.BackgroundImage = global::JuegoTragamonedas.Properties.Resources.Exit;
+            this.pictureBoxExit.Location = new System.Drawing.Point(472, 459);
+            this.pictureBoxExit.Name = "pictureBoxExit";
+            this.pictureBoxExit.Size = new System.Drawing.Size(221, 84);
+            this.pictureBoxExit.TabIndex = 9;
+            this.pictureBoxExit.TabStop = false;
+            this.pictureBoxExit.Click += new System.EventHandler(this.pictureBoxExit_Click);
+            // 
+            // MainJuego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::JuegoTragamonedas.Properties.Resources.Casino_completo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1169, 617);
+            this.Controls.Add(this.pictureBoxExit);
+            this.Controls.Add(this.pictureBoxHelp);
+            this.Controls.Add(this.imagenBotonPlay);
+            this.Controls.Add(this.pictureBoxStop3);
+            this.Controls.Add(this.pictureBoxStop2);
+            this.Controls.Add(this.pictureBoxStop1);
+            this.Controls.Add(this.panelJuego3);
+            this.Controls.Add(this.panelJuego2);
+            this.Controls.Add(this.panelJuego1);
+            this.DoubleBuffered = true;
+            this.Name = "MainJuego";
+            this.Text = "Tragamonedas";
+            this.Load += new System.EventHandler(this.MainJuego_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagenBotonPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStop3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStop2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStop1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHelp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxExit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panelJuego1;
+        private System.Windows.Forms.Panel panelJuego2;
+        private System.Windows.Forms.Panel panelJuego3;
+        private System.Windows.Forms.PictureBox pictureBoxStop1;
+        private System.Windows.Forms.PictureBox pictureBoxStop2;
+        private System.Windows.Forms.PictureBox pictureBoxStop3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox imagenBotonPlay;
+        private System.Windows.Forms.PictureBox pictureBoxHelp;
+        private System.Windows.Forms.PictureBox pictureBoxExit;
+    }
+}
+
---
+++ MainJuego.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace JuegoTragamonedas
+{
+    //Elaborado por Juan Sebastian Camargo
+    public partial class MainJuego : Form
+
+       
+    {
+        // Se crean nuevos objetivos tipo List<PictureBox>
+        List<PictureBox> 
+        GrupoFichasUno = new List<PictureBox>(),
+        GrupoFichasDos = new List<PictureBox>(),
+        GrupoFichasTres = new List<PictureBox>();
+
+        //Variables
+        int movimientoY = 0, posicionSiguiente, longitudFicha, posicionEjeX = 10;
+        bool indicador1 = false, indicador2 = false, indicador3 = false;
+        string nombreFicha1 = "", nombreFicha2 = "", nombreFicha3 = "";
+
+        public MainJuego()
+        {
+            InitializeComponent();
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            MessageBox.Show("-Bienvenidos al Slot Machine:\n-Para jugar presionar el boton 'Play'\n-Seguido a esto para detener cada una de las casillas presione los botones 'Stop' de izquierda a derecha\n(NO PRESIONAR LOS BOTONES 'Stop' SIN DARLE 'Play' ANTES)\n-Juego elaborado por: Juan Sebastian Camargo Prieto");
+
+        }
+
+
+        private void MainJuego_Load(object sender, EventArgs e)
+            //Panel que carga las imagenes al cargar el programa
+        {
+            CrearImagenes(GrupoFichasUno, panelJuego1, "Uno");
+            CrearImagenes(GrupoFichasDos, panelJuego2, "Dos");
+            CrearImagenes(GrupoFichasTres, panelJuego3, "Tres");
+                
+        }
+
+        public int NumeroFicha(string N)
+            //traer el valor del cada Ficha
+        {
+            string[] Pos = N.Split("_".ToCharArray());
+            return Convert.ToInt32(Pos[1]);
+        }
+
+
+        public List<PictureBox> CrearImagenes(List<PictureBox> Lista, Panel panel, string nombreTag) {
+        //Metodo para asignarle el valor a cada imagen y para asignarle al panel cada imagen (9 imagenes)
+        
+            longitudFicha = 190;
+            for (int i = 0; i < 9; i++) {
+                PictureBox picbox = new PictureBox();
+                picbox.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("sm" + (i + 1)); //Se traen todos los objetos que tengan el nombre 'sm' y con el 'for' se van sumando las imagenes
+                picbox.Size = new Size(longitudFicha, longitudFicha); //Declarar nuevo size para las imagenes
+                picbox.SizeMode = PictureBoxSizeMode.StretchImage; // Hacer que las imagenes queden en formato Scretch
+                picbox.Name = string.Format("{0}", i + nombreTag);
+                picbox.Tag = nombreTag + "_" + i;
+                int movim = picbox.Location.Y;
+                picbox.Location = new Point(posicionEjeX, i * longitudFicha);
+                Lista.Add(picbox);
+                panel.Controls.Add(Lista[i]);
+
+            }
+            return Lista;
+        }
+
+        public String GirarFichas(List<PictureBox> Lista, Panel panel, Timer timer, bool bandera) {
+        //Metodo para Girar las fichas con la lista
+            string fichaSeleccionada = "";
+            for (int i = 0; i < Lista.Count; i++) {
+
+                movimientoY = Lista[i].Location.Y;
+                Lista[i].Location = new Point(posicionEjeX, movimientoY - 10);
+
+                if (Lista[i].Location.Y <= -longitudFicha) {
+                    posicionSiguiente = Lista[((Lista.Count) - 1)].Location.Y;
+                    Lista[i].Location = new Point(posicionEjeX, posicionSiguiente + longitudFicha);
+                    Lista.Add(Lista[i]);
+                    panel.Controls.Add(Lista[i]);
+                    Lista.RemoveAt(i);
+
+                }
+
+                if (bandera){
+                    if (Lista[i].Location.Y == -10){
+                        timer.Stop();
+                        fichaSeleccionada = Lista[i].Tag.ToString();
+                    }
+                }
+                else {
+
+                    fichaSeleccionada = "";
+                
+                }
+            }
+
+            return fichaSeleccionada;
+        }
+
+        private void pictureBoxHelp_Click(object sender, EventArgs e)
+            //Panel Help
+        {
+            MessageBox.Show("-Bienvenidos al Slot Machine:\n-Para jugar presionar el boton 'Play'\n-Seguido a esto para detener cada una de las casillas presione los botones 'Stop' de izquierda a derecha\n(NO PRESIONAR LOS BOTONES 'Stop' SIN DARLE 'Play' ANTES)\n-Juego elaborado por: Juan Sebastian Camargo Prieto");
+        }
+
+        private void pictureBoxExit_Click(object sender, EventArgs e)
+            //Panel Exit
+        {
+            MessageBox.Show("Gracias por Jugar!");
+            this.Close();
+
+        }
+
+        //TIMERS
+        private void timer1_Tick(object sender, EventArgs e) 
+        {
+            nombreFicha1 = GirarFichas(GrupoFichasUno, panelJuego1,timer1,indicador1);
+        }
+        private void timer2_Tick(object sender, EventArgs e) 
+        {
+            nombreFicha2 = GirarFichas(GrupoFichasDos, panelJuego2, timer2, indicador2);
+        }   
+        private void timer3_Tick(object sender, EventArgs e) 
+        {
+            nombreFicha3 = GirarFichas(GrupoFichasTres, panelJuego3, timer3, indicador3);
+            if (nombreFicha3 != "")
+            { 
+                imagenBotonPlay.Image = Properties.Resources.Play_Blanco;
+                //VALIDAR GANADOR
+                if ((NumeroFicha(nombreFicha1) == NumeroFicha(nombreFicha2) && NumeroFicha(nombreFicha2) == NumeroFicha(nombreFicha3)))
+                {
+                    MessageBox.Show("Ganaste, felicidades!");
+
+                }
+                //V
+                else 
+                {
+                    MessageBox.Show("Perdiste, intenta de nuevo!");
+
+                }
+
+
+            }
+        }
+
+        //Paneles Play y Stop
+
+        private void imagenBotonPlay_Click(object sender, EventArgs e)
+        {
+            indicador1 = false;
+            indicador2 = false;
+            indicador3 = false;
+            timer1.Start();
+            imagenBotonPlay.Image = Properties.Resources.Play_Negro;
+            pictureBoxStop1.Image = Properties.Resources.Stop_Negro;
+
+
+        }
+
+        private void pictureBoxStop1_Click(object sender, EventArgs e)
+        {
+            indicador1 = true;
+            timer2.Start();
+            pictureBoxStop1.Image = Properties.Resources.Stop_Blanco;
+            pictureBoxStop2.Image = Properties.Resources.Stop_Negro;
+
+        }
+
+        private void pictureBoxStop2_Click(object sender, EventArgs e)
+        {
+            indicador2 = true;
+            timer3.Start();
+            pictureBoxStop2.Image = Properties.Resources.Stop_Blanco;
+            pictureBoxStop3.Image = Properties.Resources.Stop_Negro;
+        }
+        private void pictureBoxStop3_Click(object sender, EventArgs e)
+        {
+            indicador3 = true;
+            pictureBoxStop3.Image = Properties.Resources.Stop_Blanco;
+
+        }
+
+       
+
+        private void pictureTragaMonedas_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
